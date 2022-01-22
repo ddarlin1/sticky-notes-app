11db0c14f3e295fec1a3e8316fbeaea80ed29f9d
darling_d <ddarling212@gmail.com>
2022-01-21 19:53:15 -0500

    Organized component tree

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from "react";
+import Header from "./Header.js";
+import NotesList from "./NotesList.js";
 
 const App = () => (
   <div>
-    <header>
-      <h1>Super Sticky Notes</h1>
-      <aside>
-        <button>+ New Note</button>
-        <input type="text" placeholder="Type here to search..." />
-      </aside>
-    </header>
-    <ul>
-      <li>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description..." />
-        <span>X</span>
-      </li>
-      <li>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description..." />
-        <span>X</span>
-      </li>
-      <li>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description..." />
-        <span>X</span>
-      </li>
-    </ul>
+    <Header />
+    <NotesList />
   </div>
 );
 
